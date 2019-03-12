04210e6f04300dbc5d0d559ad3db3dbd8bddbbbf
Mike Cabana <m.cabana@ieg-america.com>
2019-03-12 14:12:44 -0400

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.get('/', async (req, res) => {
+
+    // simulate a slow response
+    const pause = new Promise((resolve) => {
+        setTimeout(() => resolve(), 2000);
+    });
+    await pause;
+
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Express Typescript starter listening on port ${PORT}...`);
+});
