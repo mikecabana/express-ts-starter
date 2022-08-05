86fbfa7d76131fd8fee32d9ff1a6a7c918c7e5e5
mikecabana <mikeyc343@gmail.com>
2022-08-05 18:03:24 -0400

    updated dependencies

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import morgan from 'morgan';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
@@ -17,15 +17,15 @@ app.use(helmet());
 app.use(cors());
 
 app.get('/', async (req, res) => {
-	// simulate a slow response
-	const pause = new Promise((resolve) => {
-		setTimeout(() => resolve(), 2000);
-	});
-	await pause;
+    // simulate a slow response
+    const pause = new Promise<null>((resolve) => {
+        setTimeout(() => resolve(null), 2000);
+    });
+    await pause;
 
-	res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-	console.log(`Express Typescript starter listening on port ${PORT}...`);
+    console.log(`Express Typescript starter listening on port ${PORT}...`);
 });
